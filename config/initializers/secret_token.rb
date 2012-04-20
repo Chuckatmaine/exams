b3c9321c6d3463a57ce9b8b8cb0382687010e5a7
chuck <chuck@rails3dev.uma.edu>
2012-04-20 10:45:58 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exams::Application.config.secret_token = '80b57d54acf3ca8fcd116266ef7e497d0ec3a4ff67ae3a11b25bf8bf00ab3b06659e4099e01d835bcedcfb8bf6d8a537ab922caaf92c80f3aad08260261573f8'
