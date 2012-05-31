6a8382502af280d475453816726b024c8739cadd
chuck <chuck@rails3dev.uma.edu>
2012-05-31 13:03:54 -0400

    ngs_to through fix

--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -1,8 +1,8 @@
 class UserAnswer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question_answer
-  belongs_to :question :through => :question_answer
-  belongs_to :test_question :through => :question_answer
-  belongs_to :test :through => :question
+  belongs_to :question
+  belongs_to :test_question
+  belongs_to :test
   attr_accessible :question_answer_id, :user_id
 end
